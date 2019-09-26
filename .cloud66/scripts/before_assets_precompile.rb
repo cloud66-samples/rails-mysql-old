bd500c9c16ed60322a462f82a87cadf5b8bea519
Dima <dima@cloud66.com>
2019-09-26 14:38:22 +0100

    Make the 11 minute wait happen during the after_checkout hook instead of during asset preocompilation

---
+++ .cloud66/scripts/before_assets_precompile.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+current_time = DateTime.now
+end_time = current_time + 11.minutes
+while current_time < end_time
+  puts "#{current_time}: performing a long asset compilation"
+  sleep(1)
+  current_time = DateTime.now
+end
