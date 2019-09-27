4a9c37226d9875d4eb6e84c47fd060ae0c022169
Dima <dima@cloud66.com>
2019-09-27 12:21:22 +0100

    Replicate SSH issue in after_checkout hook

--- .cloud66/scripts/before_assets_precompile.rb
+++ .cloud66/scripts/before_assets_precompile.rb
@@ -5,7 +5,8 @@ require "date"
 current_time = DateTime.now
 end_time = current_time + ((11.0 / 60.0) / 24.0) # 11 minutes
 while current_time < end_time
-  puts "#{current_time}: performing a long asset compilation"
+  File.open("/tmp/asset-compilation", "a") { |f| f.write("#{current_time}: performing a long asset compilation\n") }
+  # puts "#{current_time}: performing a long asset compilation"
   sleep(1)
   current_time = DateTime.now
 end
