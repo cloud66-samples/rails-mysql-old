c4b7e4f52d071c3752a804b436f55790a6770c78
Dima <dima@cloud66.com>
2020-04-22 14:41:31 +0100

    Yes

--- config/initializers/stop_app_from_loading.rb
+++ config/initializers/stop_app_from_loading.rb
@@ -1,2 +1,2 @@
 Rails.logger.info("$0 is #{$0} for PID #{$$}")
-raise "STOPPING APP FROM LOADING" if $0.include?("puma") && !$0.include?("releases")
+raise "STOPPING APP FROM LOADING" if $0.include?("puma") && !$0.include?("rake")
